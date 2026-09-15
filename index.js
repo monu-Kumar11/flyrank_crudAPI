e70c3d7a3bb29681d28ccd87d6d2cabe21b93a2e
Monu Kumar <monubtech1@gmail.com>
2026-09-15 12:42:03 +0530

    Stage 0: hello server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Stage 0: Hello, server
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, server!');
+});
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
